ae3482d2dc32b571d00287903a2cdfae059eb8bf
kevinmstapleton <kevinmstapleton@gmail.com>
2023-05-22 18:06:05 -0500

    greatly improved the make file with further options and a safety on the clear function

--- make.bat
+++ make.bat
@@ -13,8 +13,15 @@ set CGO_ENABLED=0
 
 
 IF "%1" == "clean" (
-	echo cleaning previous executables
-	for /F "delims=" %%i in ('dir %~dp0\bin') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+	echo cleaning previous executables...
+	for /f tokens^=* %%i in ("dir %~dp0\bin") do echo/ Path: %%~dpi ^| Name: %%~nxi
+	set /p "choice= Are you sure you want to delete ALL files in the bin folder?: " 
+	echo choice: '!choice!'
+	IF NOT "!choice!" EQU "y" (
+	echo have a good day^^!
+	EXIT /B 1
+	)
+	DEL "%~dp0\bin\*" /S /Q
 	echo Finished Cleaning.
 	EXIT /B 0
 )
@@ -31,21 +38,34 @@ git log --oneline --decorate > CHANGELOG
 echo Changelog Build Finished
 echo.
 
+IF "%1" == "" (
+	echo Please select a file to compile:
+	echo ** atomic-harness **	
+	echo ** atrutil **	
+	echo ** all ** 
+	echo ** clean **
+	EXIT /B 0
+)
+
   for /F "tokens=2 delims==" %%s in ('set ARCH[') do (
-    
+ 
+
     SET GOOS=windows
     SET GOARCH=%%s
     SET GOARM=6
     echo Building !GOOS!/!GOARCH!
 
     SET EXTENSION=".exe"
-
-      go build -buildmode=exe -ldflags "-s -w -X main.version=%@version% -X main.buildstamp=%@bdate%-%@btime% -X main.hash=%@gitrev%" -o bin/atomic-harness_!GOARCH!!EXTENSION! ./cmd/harness/
-      go build -buildmode=exe -ldflags "-s -w -X main.version=%@version% -X main.buildstamp=%@bdate%-%@btime% -X main.hash=%@gitrev%" -o bin/atrutil_!GOARCH!!EXTENSION! ./cmd/atrutil
-   
-   echo.
-  )
+	IF "%1" == "all" (
+      	go build -buildmode=exe -ldflags "-s -w -X main.version=%@version% -X main.buildstamp=%@bdate%-%@btime% -X main.hash=%@gitrev%" -o bin/atomic-harness_!GOARCH!!EXTENSION! ./cmd/harness/
+      	go build -buildmode=exe -ldflags "-s -w -X main.version=%@version% -X main.buildstamp=%@bdate%-%@btime% -X main.hash=%@gitrev%" -o bin/atrutil_!GOARCH!!EXTENSION! ./cmd/atrutil
+   	)
+ 	IF "%1" == "atrutil" (
+      	go build -buildmode=exe -ldflags "-s -w -X main.version=%@version% -X main.buildstamp=%@bdate%-%@btime% -X main.hash=%@gitrev%" -o bin/atrutil_!GOARCH!!EXTENSION! ./cmd/atrutil
+   	)
+	IF "%1" == "atomic-harness" (
+      		go build -buildmode=exe -ldflags "-s -w -X main.version=%@version% -X main.buildstamp=%@bdate%-%@btime% -X main.hash=%@gitrev%" -o bin/atomic-harness_!GOARCH!!EXTENSION! ./cmd/harness/
+   	)
+   )
 )
-
-echo.
 echo Finished Building
