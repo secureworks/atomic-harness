1c5846103527b0ce6c1d3099761a0af014e31856
Alex M <58046369+amalone-scwx@users.noreply.github.com>
2023-05-23 06:45:32 -0700

    update fetch_deps - goartrun no longer external

--- scripts/fetch_deps.sh
+++ scripts/fetch_deps.sh
@@ -3,17 +3,6 @@ SCRIPTSDIR=$(readlink -f $(dirname $0))
 
 pushd "${SCRIPTSDIR}/../.."
 
-# tools
-
-if [ ! -d goartrun ] ; then
-	git clone git@github.com:secureworks/goartrun \
-	&& cd goartrun && make ; cd ..
-fi
-
-if [ ! -d telemetry-tool-example ] ; then
-	git clone git@github.com:secureworks/telemetry-tool-example.git
-fi
-
 # tests
 
 if [ ! -d atomic-red-team ] ; then
@@ -26,3 +15,8 @@ if [ ! -d atomic-validation-criteria ] ; then
 	git clone git@github.com:secureworks/atomic-validation-criteria.git
 fi
 
+# tools
+
+if [ ! -d telemetry-tool-example ] ; then
+	git clone git@github.com:secureworks/telemetry-tool-example.git
+fi
